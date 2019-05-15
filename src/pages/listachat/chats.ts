6c631369e51ed5350ea4d9fa3e9e98e7be95ea13
Lucas Uesato Queiroz <lucas.uesato@funeda.com.br>
2019-05-15 19:47:04 -0300

    listachatok

--- src/pages/listachat/chats.ts
+++ src/pages/listachat/chats.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { IonicPage, NavController, NavParams, LoadingController } from "ionic-angular";
 import { AngularFireDatabase } from 'angularfire2/database';
 import { UsuarioProvider } from '../../providers/usuario/usuario';
 import { Usuarios } from '../../models/Usuarios';
-import { Observable } from "rxjs/Observable";
 import * as _ from "lodash";
 
-import { map } from "rxjs/operators";
-import { Chat } from "../../models/Chat"
 import { ChatService } from "../../app/app.service";
-import { ChatroomPage } from "../chatroom/chatroom";
 
 /**
  * Generated class for the ChatsPage page.
@@ -36,7 +32,8 @@ export class ChatsPage implements OnInit {
     private db: AngularFireDatabase,
     private provider: UsuarioProvider,
     private currrentUser: Usuarios,
-    private chatService: ChatService
+    private chatService: ChatService,
+    private loadingCtrl: LoadingController
   ) { }
 
   ionViewDidLoad() {
@@ -45,7 +42,7 @@ export class ChatsPage implements OnInit {
 
   ngOnInit() {
     var chats: any[] = [];
-    
+
     return this.db.object('/chats').snapshotChanges().map(c => {
       return { key: c.key, ...c.payload.val() };
 
@@ -53,7 +50,6 @@ export class ChatsPage implements OnInit {
 
       Object.keys(res).forEach(key => {
         var msg = new Object({ key, ...res[key] });
-        console.log(msg);
         chats.push(msg);
       });
       chats = chats.slice(1);
@@ -62,43 +58,52 @@ export class ChatsPage implements OnInit {
 
   }
 
-  pegarUsuariosConectados(list) { 
+  pegarUsuariosConectados(list) {
+    const loading = this.loadingCtrl.create({
+      spinner: 'dots',
+      content: 'Carregando...'
+    });
+
+    loading.present();
     for (let obj of list) {
       var users = obj.pair.split("|", 2);
       var user1 = users[0];
       var user2 = users[1];
       if (user1 == this.currrentUser.id) {
         this.addUsuarioLista(user2);
-      } else if(user2 == this.currrentUser.id){        
-        this.addUsuarioLista(user1);     
+      } else if (user2 == this.currrentUser.id) {
+        this.addUsuarioLista(user1);
       }
     }
+    loading.dismiss();
   }
 
-    addUsuarioLista(id){
-      var allusers: any[] = [];
-      allusers = this.provider.getAll();
-      console.log(this.provider.get(id));
-      debugger;
-      if(!this.availableusers.some(x => x === id)){
-        this.availableusers.push(this.filtrausers(allusers,id));  
-      }          
-    }      
-      
-    filtrausers(chatsToFilter , id) {
-      var usuario;
-      this.filtros["id"] = val => val == id;
-      debugger;
-      usuario = _.filter(chatsToFilter, _.conforms(this.filtros));
-      return usuario.id;
+  addUsuarioLista(id) {
+    this.provider.get(id).subscribe(async (data) => {
+      var user: any;
+      user = data;
+      user.id = data.key;
+      if (user.id != this.currrentUser.id && !this.verifyUser(user, this.availableusers)) {
+        this.availableusers.push(user);
+      }
+    });
+  }
+
+  verifyUser(user, list) {
+    var user = user.id;
+    for (let obj of list) {
+      if (user === obj.id) {
+        return true;
+      }
     }
+    return false;
+  }
 
   goToChat(chatpartner) {
-    chatpartner.id = chatpartner.key;
 
     this.chatService.currentChatPairId = this.chatService.createPairId(this.currrentUser, chatpartner);
     this.chatService.currentChatPartner = chatpartner;
 
-    this.navCtrl.push(ChatroomPage);
+    this.navCtrl.push("ChatroomPage");
   } //goToChat
 }
